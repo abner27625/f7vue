4184b077d045b0cce5fd398d88e0361f81f645c9
songheng <729818876@qq.com>
2017-07-19 17:06:25 +0800

    init

--- myapp/config/index.js
+++ myapp/config/index.js
@@ -4,9 +4,12 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsPublicPath: '/',
+    //index: path.resolve(__dirname, '../dist/index.html'),
+    //assetsRoot: path.resolve(__dirname, '../dist'),
+    //assetsPublicPath: '/',
+    index: path.resolve(__dirname, '../../www/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../www'),
+    assetsPublicPath: '',
     assetsSubDirectory: 'static',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
