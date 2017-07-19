efdc627a78dca0b2ff0a57e0b60ba09cf7c64c84
songheng <729818876@qq.com>
2017-07-19 17:07:20 +0800

    init

--- myapp/config/index.js
+++ myapp/config/index.js
@@ -10,6 +10,7 @@ module.exports = {
     index: path.resolve(__dirname, '../../www/index.html'),
     assetsRoot: path.resolve(__dirname, '../../www'),
     assetsPublicPath: '',
+
     assetsSubDirectory: 'static',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
